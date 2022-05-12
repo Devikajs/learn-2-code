ca6e7d86f19b090b0924ac373411e149749ea7f9
Devika js <96069055+Devikajs@users.noreply.github.com>
2022-05-12 20:57:30 +0530

    Create remainder remainder.c

---
+++ remainder remainder.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {int a=7,b=3,c=10,e=9;
+            scanf("%d\n",&a);
+                scanf("%d\n",&b);
+            scanf("%d\n",&c);
+            scanf("%d\n",&e);
+            printf("%d",a%b+c%e);
+            
+            
