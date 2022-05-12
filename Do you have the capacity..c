efdb867da8b312454383c7bf40f9410e7b11a7ea
Devika js <96069055+Devikajs@users.noreply.github.com>
2022-05-12 20:59:16 +0530

    Create Do you have the capacity..c

---
+++ Do you have the capacity..c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+   float l=4.2,b=7.4,c=9.1;
+    scanf("%f\n",&l);
+    scanf("%f\n",&b);
+    scanf("%f\n",&c);
+    printf("%0.3f\n",l*l*l);
+    printf("%0.3f\n",l*b*c);
+       
+    return 0;
+}
