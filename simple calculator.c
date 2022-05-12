76ad64528cb0c9aea370d2d7097dd110205265a3
Devika js <96069055+Devikajs@users.noreply.github.com>
2022-05-12 20:56:41 +0530

    Create simple calculator.c

---
+++ simple calculator.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() { int a,b;
+            scanf("%d\n",&a);
+            scanf("%d\n",&b);
+            printf("%d\n",a+b);
+            printf("%d\n",a-b);
+            printf("%d\n",a*b);
+            
+            printf("%0.2f\n",(float)a/b);
+                
