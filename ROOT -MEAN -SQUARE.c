5594e68b6ae186d58f43b7539d4e1854449b2112
Devika js <96069055+Devikajs@users.noreply.github.com>
2022-05-12 21:00:28 +0530

    Create ROOT -MEAN -SQUARE.c

---
+++ ROOT -MEAN -SQUARE.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {int a=8,b=-6,c=3;
+            scanf("%d\n",&a);
+            scanf("%d\n",&b);
+            scanf("%d\n",&c);
+          printf("%0.2f\n",(float)sqrt((float)(a*a+b*b+c*c)/3));
+
+        
+    return 0;
+}
