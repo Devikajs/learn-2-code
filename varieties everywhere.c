433a20625722d2692e8eb905bf2e1e5238bdb87f
Devika js <96069055+Devikajs@users.noreply.github.com>
2022-05-12 20:55:24 +0530

    Create varieties everywhere.c

---
+++ varieties everywhere.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() { int a=1;
+           
+           scanf("%d\n",&a);
+            char w;
+            
+            scanf("%c\n",&w);
+                float c=2.5;
+           
+            scanf("%f",&c);
+            printf("%d\n",a);
+            printf("%c\n",w);
+            printf("%f\n",c);
+               
+    return 0;
+}
