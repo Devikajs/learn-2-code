11bd31dbd2786c469b9e489fa0dde9a886f3a802
Devika js <96069055+Devikajs@users.noreply.github.com>
2022-06-28 08:27:50 +0530

    Create pointers in strings.c

---
+++ pointers in strings.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+ char str[100];
+    char *p;
+    fgets(str,100,stdin);
+    p=str;
+    while(*p!='\0')
+        printf("%c",*p++);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
