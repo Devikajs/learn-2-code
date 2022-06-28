1a5a94bad3b60c79262c547c513c13975a668f98
Devika js <96069055+Devikajs@users.noreply.github.com>
2022-06-28 08:17:46 +0530

    Create Character frequency.c

---
+++ Character frequency.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    char st[100], ele;
+    int count = 0;
+   //scanf("%s",st);
+   fgets(st, sizeof(st), stdin);
+  
+
+    
+    scanf("%c", &ele);
+
+    for (int i = 0; st[i] != '\0'; ++i) {
+        if (ele == st[i])
+            count++;
+    }
+    
+    count!=0?printf("%d",count):printf("Element not present.");
+    
+    return 0;
+}
